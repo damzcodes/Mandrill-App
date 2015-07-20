2b854ae68de08e3254b6458d2c1c1118ef268da9
dami livelink <d.livelink@Dami.local>
2015-07-20 17:54:02 +0100

    Add vcard details & attachment

--- app.rb
+++ app.rb
@@ -8,19 +8,39 @@ end
 
 # This sends an emaili to a given email
 post '/' do
-  m = Mandrill::API.new 'RXuMBikZdJ2Zhvd_FdrM5Q'
+  m = Mandrill::API.new 'asEPl1ngRJzo6FdKFQhsgw'
 
   message = {  
-    :subject=> "Hello from the Mandrill API",  
-    :from_name=> "Your name",  
+    :subject=> "Damilola Odelola business card",  
+    :from_name=> "Damilola Odelola",  
     :to=>[
       {  
-       :email=> "#{params["email"]}",  
-       :name=> "Recipient1"  
+       :email=> "#{params["email"]}" 
       }  
-    ],  
-    :html=>"<html><h1>Hi this is a test</h1></html>",  
-    :from_email=>"d.odelola@livelinktechnology.net"  
+    ],
+    :attachments => [{
+      "type" => "text/vcard",
+      "name" => "Damilola Odelola.vcf",
+      "content" => "QkVHSU46VkNBUkQNClZFUlNJT046My4wDQpQUk9ESUQ6LS8vQXBwbGUgSW5jLi8vTWFjIE9TIFg
+      gMTAuMTAvL0VODQpOOk9kZWxvbGE7RGFtaWxvbGE7OzsNCkZOOkRhbWlsb2xhIE9kZWxvbGENCk
+      9SRzp3cml0ZXIgfCBkZXZlbG9wZXIgfCBmb3VuZGVyOw0KRU1BSUw7dHlwZT1JTlRFUk5FVDt0e
+      XBlPUhPTUU7dHlwZT1wcmVmOm1lQGRhbWlsb2xhb2RlbG9sYS5jb20NCml0ZW0xLlVSTDt0eXBl
+      PXByZWY6d3d3LmRhbWlsb2xhb2RlbG9sYS5jb20NCml0ZW0xLlgtQUJMYWJlbDpfJCE8SG9tZVB
+      hZ2U+ISRfDQppdGVtMi5VUkw6d3d3LmJsYWNrZ2lybC50ZWNoDQppdGVtMi5YLUFCTGFiZWw6Ym
+      xhY2tnaXJsLnRlY2gNCml0ZW0zLlVSTDp3d3cudHdpdHRlci5jb20vZGFtaW9kZWxvbGENCml0Z
+      W0zLlgtQUJMYWJlbDp0d2l0dGVyDQpYLUFCVUlEOjlEOUJEREM3LUU2QkMtNDgxNC05MDY4LUUy
+      OTgzNEE5QjkyMzpBQlBlcnNvbg0KRU5EOlZDQVJEDQo="
+      }], 
+    :html=>"<html><p>Hi,<br>
+    This email is being sent to you because: <br>
+    <ol>
+      <li> You asked me to </li>
+      <li> You want a vcard version of my contact details (in the event you lose the business card or drown your wallet in a puddle)</li>
+    </ol>
+    Feel free to download my details onto your phone or other digital device.<br>
+    Best, <br>
+    Damilola Odelola</p></html>",  
+    :from_email=>"me@damilolaodelola.com"  
   }  
   sending = m.messages.send message  
   puts sending
